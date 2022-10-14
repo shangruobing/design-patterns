8849a23c93d0f3a8a8ddd93800a376d28f08192f
Ruobing Shang <shangruobing29@163.com>
2022-10-14 16:21:15 +0800

    Learned the Template Method.

---
+++ src/templatemathod/MainApp.java
@@ -0,0 +1,13 @@
+package templatemathod;
+
+/**
+ * @author Ruobing Shang 2022-10-14 15:56
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        DataMiner jsonDataMiner = new JsonDataMiner();
+        jsonDataMiner.mine();
+        DataMiner xmlDataMiner = new XmlDataMiner();
+        xmlDataMiner.mine();
+    }
+}
